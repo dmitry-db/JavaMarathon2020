e5eb43dab34ddc072cc67cf0ad1e0f8689f2355a
Dmitry Borodachev <borodachev.58@yandex.ru>
2020-11-30 11:37:12 +0300

    День 1 все задания выполнены

--- src/main/java/day0/Task1.java
+++ src/main/java/day0/Task1.java
@@ -3,5 +3,6 @@ package day0;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Task 1 completed");
+        System.out.println("Task 1 completed");
     }
 }
