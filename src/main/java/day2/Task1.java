77f957796e58351dc83d48e06c07970a7035787f
Dmitry Borodachev <borodachev.58@yandex.ru>
2020-12-01 14:56:38 +0300

    День 2 все задания выполнены

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,22 @@
 package day2;
+import java.util.Scanner;
+
 
 public class Task1 {
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int et = sc.nextInt();
+
+    if (et >= 1 && et <= 4){
+        System.out.println("Малоэтажный дом");
+    } else if(et >= 5 && et <= 8){
+        System.out.println("Среднеэтажный дом");
+    } else if (et >= 9){
+        System.out.println("Многоэтажный дом");
+    } else {
+        System.out.println("Ошибка ввода");
+    }
+
 
     }
 }
--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,29 @@
 package day2;
+import com.sun.xml.bind.v2.runtime.output.SAXOutput;
+
+import java.util.Scanner;
+
 
 public class Task2 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
+
+        //System.out.println("Введите число a");
+        int a = s.nextInt();
+       // System.out.println("Введите число b");
+        int b = s.nextInt();
+
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        } else {
+            for (int i = a + 1; i < b; i++) {
+                if (i % 5 == 0 && i % 10 != 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
     }
+
 }
+
