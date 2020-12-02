af164cca4a2d3c1cc80faa151905a9764eb5a560
Dmitry Borodachev <borodachev.58@yandex.ru>
2020-12-02 13:15:07 +0300

    День 2 все задания выполнены

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,47 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s;
+        do {
+            s = scanner.nextLine();
+            switch (s){
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                case "Россия":
+                    System.out.println("Россия");
+                    break;
 
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                case "Италия":
+                    System.out.println("Италия");
+                    break;
+
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                case "Англия":
+                    System.out.println("Англия");
+                    break;
+
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                case "Германия":
+                    System.out.println("Германия");
+                    break;
+                case "Stop":
+                    break;
+
+                default:
+                    System.out.println("Неизвестная страна");
+            }
+        } while (!s.equals("Stop"));
     }
 }
