4c63d53aea7ad5df168c1f24fe7c5211977cc935
Dmitry Borodachev <borodachev.58@yandex.ru>
2020-12-04 16:00:18 +0300

    День 5 все задания выполнены

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,14 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
 
+        car.setYear(2010);
+        car.setColor("green");
+        car.setModel("bmw");
+
+        System.out.println("Год выпуска - " + car.getYear());
+        System.out.println("Цвет - " + car.getColor());
+        System.out.println("Модель - " + car.getModel());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,10 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(2005, "yellow", "zvezda");
 
+        System.out.println("Год выпуска - " + motorbike.getYear());
+        System.out.println("Цвет - " + motorbike.getColor());
+        System.out.println("Модель - " + motorbike.getModel());
     }
 }
