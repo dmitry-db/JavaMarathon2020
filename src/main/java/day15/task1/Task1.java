2f6bb3549cedda312534d950c800b77b6d0f044a
Dmitry Borodachev <borodachev.58@yandex.ru>
2020-12-15 13:24:07 +0300

    День 15 выполнил

--- src/main/java/day15/task1/Task1.java
+++ src/main/java/day15/task1/Task1.java
@@ -1,7 +1,30 @@
 package day15.task1;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:/Users/Dmitry/javaprojects/JavaMarathon2020/src/main/resources/shoes.csv");
+        File file1 = new File("test15");
+
+        Scanner scanner = new Scanner(file);
+        PrintWriter pr = new PrintWriter(file1);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] s = line.split(";");
+            if (Integer.parseInt(s[2]) == 0) {
+                pr.println(line);
+            }
+        }
+
+        pr.flush();
+        pr.close();
+        scanner.close();
     }
+
 }
+
