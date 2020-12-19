e16468622f20f882f0ca1f84ab58c43247e350d5
Dmitry Borodachev <borodachev.58@yandex.ru>
2020-12-19 20:13:04 +0300

    День 18 выполнил

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -2,6 +2,14 @@ package day18;
 
 public class Task1 {
     public static void main(String[] args) {
+        int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+        System.out.println(recursionSum(numbers, numbers.length));
+    }
 
+    public static int recursionSum(int[] s, int size) {
+        if (size > 0){
+            return s[size - 1] + recursionSum(s, size - 1);
+        }
+        return 0;
     }
 }
